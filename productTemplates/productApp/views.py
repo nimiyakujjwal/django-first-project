641a164990357303b0aceda3a933cf3c4d61df25
Ujjwal Kumar <26951554+nimiyakujjwal@users.noreply.github.com>
2026-05-17 20:19:58 +0530

    Create a Product App with Templates
    Basic Django project setup with a product app and templates for displaying products.
    The project includes views for electronics, toys, and shoes, and a main index page that links to these categories.
    	modified:   productApp/views.py
    	modified:   productTemplates/settings.py
    	modified:   productTemplates/urls.py
    	new file:   templates/productApp/index.html
    	new file:   templates/productApp/products.html

--- productTemplates/productApp/views.py
+++ productTemplates/productApp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+def electronics(request):
+    product_dict = {
+        'product1': 'Laptop',
+        'product2': 'Smartphone',
+        'product3': 'Tablet'
+    }
+    return render(request, 'productApp/products.html', context=product_dict)
+
+def toys(request):
+    product_dict = {
+        'product1': 'Toy Car',
+        'product2': 'Doll',
+        'product3': 'Puzzle'
+    }
+    return render(request, 'productApp/products.html', context=product_dict)
+
+def shoes(request):
+    product_dict = {
+        'product1': 'Sneakers',
+        'product2': 'Boots',
+        'product3': 'Sandals'
+    }
+    return render(request, 'productApp/products.html', context=product_dict)
+
+def index(request):
+    return render(request, 'productApp/index.html')
--- productTemplates/productTemplates/urls.py
+++ productTemplates/productTemplates/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from productApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('productapp/electronics/', views.electronics, name='electronics'),
+    path('productapp/toys/', views.toys, name='toys'),
+    path('productapp/shoes/', views.shoes, name='shoes'),
+    path('', views.index, name='index')
 ]
